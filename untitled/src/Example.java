8b5832812d5e9280ecc466313273b04087b6ac55
galina <galina.krupitsa@gmail.com>
2025-08-20 16:43:22 +0500

    w

--- untitled/src/Example.java
+++ untitled/src/Example.java
@@ -1,16 +1,17 @@
 @MyAnnotation(value = "ExampleClass", version = 2)
+@Ann2(value = "Аннотация2")
 public class Example {
 
     @MyAnnotation("exampleField")
     private String field;
-    @MyAnnotation("exampleField2")
+    @MyAnnotation("Поле 2")
     private int value;
 
     @MyAnnotation(value = "doSomethingMethod")
     public void doSomething() {
         System.out.println("Doing something ...");
     }
-    @MyAnnotation(value = "PrintHelloWorld")
+    @Ann2(value = "Это аннотация 2 к методу 2")
     public void PrintHelloWorld() {
         System.out.println("Hello World");
 
