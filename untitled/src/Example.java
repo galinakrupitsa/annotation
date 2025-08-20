aa0462ed7bf47b159c4f5ba3fcf32af4cb15d2a4
galina <galina.krupitsa@gmail.com>
2025-08-20 16:23:02 +0500

    w

--- untitled/src/Example.java
+++ untitled/src/Example.java
@@ -3,9 +3,16 @@ public class Example {
 
     @MyAnnotation("exampleField")
     private String field;
+    @MyAnnotation("exampleField2")
+    private int value;
 
     @MyAnnotation(value = "doSomethingMethod")
     public void doSomething() {
-        System.out.println("Doing something...");
+        System.out.println("Doing something ...");
+    }
+    @MyAnnotation(value = "PrintHelloWorld")
+    public void PrintHelloWorld() {
+        System.out.println("Hello World");
+
     }
 }
